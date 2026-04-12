48fe305fbf488e1e15c4f2392c2149d3637c1420
Viktor Lošťák <viktor.lostak@radixal.net>
2026-04-12 12:07:30 +0200

    fix: Re-generate data.js into correct docs folder

---
+++ docs/vizualizace/data.js
@@ -0,0 +1,118 @@
+const simulationData = {
+  "ElitistData": [
+    {
+      "Generation": 1,
+      "MedianAvgIq": 119.48603945054471,
+      "MedianMaxIq": 135.62452333612998,
+      "MaxIqP5": 120.2239569162901,
+      "MaxIqP95": 151.41889150266167,
+      "MedianGeneticDiversity": 0.7329215724070821
+    },
+    {
+      "Generation": 2,
+      "MedianAvgIq": 127.76623929619538,
+      "MedianMaxIq": 141.67972304499045,
+      "MaxIqP5": 120.7854874911803,
+      "MaxIqP95": 157.864250248921,
+      "MedianGeneticDiversity": 0.5705940126776474
+    },
+    {
+      "Generation": 3,
+      "MedianAvgIq": 133.17700103834395,
+      "MedianMaxIq": 144.15306935171836,
+      "MaxIqP5": 124.98389764424797,
+      "MaxIqP95": 161.58327084021187,
+      "MedianGeneticDiversity": 0.49269973950597057
+    },
+    {
+      "Generation": 4,
+      "MedianAvgIq": 136.29698832610546,
+      "MedianMaxIq": 147.69886856232665,
+      "MaxIqP5": 124.83590859311784,
+      "MaxIqP95": 169.91984999363797,
+      "MedianGeneticDiversity": 0.41617651608696116
+    },
+    {
+      "Generation": 5,
+      "MedianAvgIq": 141.99698101598239,
+      "MedianMaxIq": 150.04587046612068,
+      "MaxIqP5": 125.79719437594787,
+      "MaxIqP95": 173.14440956433504,
+      "MedianGeneticDiversity": 0.36207327142687473
+    },
+    {
+      "Generation": 6,
+      "MedianAvgIq": 144.47902356037287,
+      "MedianMaxIq": 150.2261137825754,
+      "MaxIqP5": 123.66775008721798,
+      "MaxIqP95": 176.85576572161273,
+      "MedianGeneticDiversity": 0.30437958193893233
+    },
+    {
+      "Generation": 7,
+      "MedianAvgIq": 148.1159957467474,
+      "MedianMaxIq": 153.77458301662023,
+      "MaxIqP5": 128.4903447072865,
+      "MaxIqP95": 178.75330493025518,
+      "MedianGeneticDiversity": 0.2766123527003779
+    }
+  ],
+  "HalfInbreedingData": [
+    {
+      "Generation": 1,
+      "MedianAvgIq": 117.9002346336678,
+      "MedianMaxIq": 134.35164746712314,
+      "MaxIqP5": 117.39634517148411,
+      "MaxIqP95": 149.4989416633948,
+      "MedianGeneticDiversity": 0.7408768160516466
+    },
+    {
+      "Generation": 2,
+      "MedianAvgIq": 123.02451919360527,
+      "MedianMaxIq": 137.6104597131266,
+      "MaxIqP5": 125.10451477709883,
+      "MaxIqP95": 154.79609136441258,
+      "MedianGeneticDiversity": 0.7272574079025076
+    },
+    {
+      "Generation": 3,
+      "MedianAvgIq": 129.17692917381834,
+      "MedianMaxIq": 140.67916279252407,
+      "MaxIqP5": 127.5652498351593,
+      "MaxIqP95": 160.59037611378778,
+      "MedianGeneticDiversity": 0.5964492625773585
+    },
+    {
+      "Generation": 4,
+      "MedianAvgIq": 131.04835585007507,
+      "MedianMaxIq": 142.28743971955396,
+      "MaxIqP5": 116.59881612711342,
+      "MaxIqP95": 164.96619364643502,
+      "MedianGeneticDiversity": 0.5764879846609882
+    },
+    {
+      "Generation": 5,
+      "MedianAvgIq": 135.11075343621917,
+      "MedianMaxIq": 146.8497000231958,
+      "MaxIqP5": 124.05406636559277,
+      "MaxIqP95": 164.22597483344884,
+      "MedianGeneticDiversity": 0.505055783729607
+    },
+    {
+      "Generation": 6,
+      "MedianAvgIq": 135.56911142599643,
+      "MedianMaxIq": 146.17999307482634,
+      "MaxIqP5": 124.07449645348223,
+      "MaxIqP95": 166.32462280173183,
+      "MedianGeneticDiversity": 0.4898578793156153
+    },
+    {
+      "Generation": 7,
+      "MedianAvgIq": 138.45533087491202,
+      "MedianMaxIq": 147.7456003939737,
+      "MaxIqP5": 124.66141339070086,
+      "MaxIqP95": 171.54358450008186,
+      "MedianGeneticDiversity": 0.39972767056113645
+    }
+  ]
+};
